bd50ff8af9ee1466e5ca36654ac8c703bf89110e
Romuald Brillout <git@brillout.com>
2024-04-22 21:19:11 +0200

    fix: improve CLI help

--- src/cli.ts
+++ src/cli.ts
@@ -1,29 +1,48 @@
-import { Command, program } from 'commander'
+import { program } from 'commander'
 import { type Args, releaseMe, type ReleaseType, releaseTypes, type ReleaseTarget } from './releaseMe'
 
-const packageRootDir = process.cwd()
-const args = parseArgs()
-releaseMe(args, packageRootDir)
+const helpText = [
+  '',
+  'Examples:',
+  '  $ pnpm exec release-me patch                        # bump patch semver',
+  '  $ pnpm exec release-me minor                        # bump minor semver',
+  '  $ pnpm exec release-me major                        # bump major semver',
+  '  $ pnpm exec release-me commit                       # out-of-band release (as x.y.z-commit-123456 without npm tag)',
+  '  $ pnpm exec release-me v${major}.${minor}.${patch}  # release specific version',
+].join('\n')
+
+cli()
+
+function cli() {
+  const packageRootDir = process.cwd()
+  const args = parseArgs()
+  releaseMe(args, packageRootDir)
+}
 
 function parseArgs(): Args {
-  program.name('release-me').option('--force').argument('<release-target>')
+  program
+    .name('release-me')
+    .option('--force', "Release even if the current commit (i.e. HEAD) isn't up-to-date with origin/main")
+    .argument('<release-target>')
+  program.addHelpText('afterAll', helpText)
+  program.showHelpAfterError()
   program.parse()
   // At this point, program.args contains the positional arguments and program.opts() contains the flags.
 
   if (program.args.length !== 1) {
-    showUsage(program)
+    program.outputHelp()
     process.exit(0)
   }
 
   const releaseTarget = program.args[0]
   if (!releaseTarget) {
     console.error('Wrong CLI usage: argument is missing.')
-    showUsage(program)
+    program.outputHelp()
     process.exit(0)
   }
   if (!isValidReleaseTarget(releaseTarget)) {
     console.error('Wrong CLI usage: argument is invalid.')
-    showUsage(program)
+    program.outputHelp()
     process.exit(0)
   }
 
@@ -31,22 +50,6 @@ function parseArgs(): Args {
     force: (program.opts().force as boolean) || false,
     releaseTarget,
   }
-}
-
-function showUsage(program: Command) {
-  program.outputHelp()
-  console.log(
-    [
-      '',
-      'Examples:',
-      '  $ pnpm exec release-me patch                        # bump patch semver',
-      '  $ pnpm exec release-me minor                        # bump minor semver',
-      '  $ pnpm exec release-me major                        # bump major semver',
-      '  $ pnpm exec release-me commit                       # out-of-band release (as x.y.z-commit-123456 without npm tag)',
-      '  $ pnpm exec release-me v${major}.${minor}.${patch}  # release specific version',
-      '  $ pnpm exec release-me --git-prefix my-pkg patch    # git tag prefix -> my-pkg@${major}.${minor}.${patch}',
-    ].join('\n'),
-  )
 }
 
 function isValidReleaseTarget(releaseTarget: string): releaseTarget is ReleaseTarget {
