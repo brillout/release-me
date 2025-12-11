1b84d24adc00507988c3bc48c1459aa736c260fa
Romuald Brillout <git@brillout.com>
2025-12-11 21:51:00 +0100

    use npm publish instead of pnpm publish

--- src/releaseMe.ts
+++ src/releaseMe.ts
@@ -231,9 +231,9 @@ async function npmPublishBoilerplates(boilerplatePackageJson: string) {
   await npmPublish(path.dirname(boilerplatePackageJson))
 }
 async function npmPublish(dir: string, tag?: string) {
-  logTitle('Pnpm publish')
+  logTitle('npm publish')
   const env = getNpmFix()
-  let cmd = 'pnpm publish --no-git-checks'
+  let cmd = 'npm publish --no-git-checks'
   if (tag) {
     cmd = `${cmd} --tag ${tag}`
   }
